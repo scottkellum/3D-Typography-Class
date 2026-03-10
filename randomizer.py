2a80b19166589fd69eb4b9379298cac542bec97e
Rob Stenson <rob@goodhertz.com>
2026-03-10 15:28:19 -0700

    latest for class 3

---
+++ randomizer.py
@@ -0,0 +1,21 @@
+from coldtype import *
+from random import Random
+
+names = ["bert", "anna rose", "zeh", "zack", "scott", "seth", "adam", "wendrich", "stephen", "jennifer", "kirstin", "benjamin", "alec"]
+
+rndm = Random()
+rndm.seed(1)
+rndm.shuffle(names)
+
+@animation(tl=len(names), bg=0)
+def namer(f:Frame):
+    return (StSt(names[f.i], "ObviouslyV", 200
+        , wght=1
+        , slnt=1
+        , wdth=1
+        , fit=f.a.r.w-100
+        , case="upper")
+        .f(1)
+        .layer([7, lambda i, p: p.t(-i*60, 0)])
+        .stack(11)
+        .align(f.a.r))
